dfb7b155aac3955d0105d5179ac336fa8eb498f7
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-04-20 20:38:48 -0300

    Implemented Audio Manager fixed #1

---
+++ Assets/Script/Controllers/AudioManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource MusicEnvironment;
+    public AudioSource SoundFX;
+
+    //Lazy Singleton
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance == null)
+            Instance = this;
+        else if(Instance != this)
+            Destroy(gameObject);
+
+        GetPlayerPrefs();
+    }
+
+    /// <summary>
+    /// Load the player prefs volume values
+    /// </summary>
+    private void GetPlayerPrefs()
+    {
+        SetMusicVolume(PlayerPrefs.HasKey("Music") ? PlayerPrefs.GetFloat("Music") : 1);
+        SetSoundFXVolume(PlayerPrefs.HasKey("SoundFX") ? PlayerPrefs.GetFloat("SoundFX") : 1);
+    }
+
+    /// <summary>
+    /// Play a SoundFX clip
+    /// </summary>
+    /// <param name="clip">Clip.</param>
+    public void PlayClip(AudioClip clip)
+    {
+        ChangeClip(clip);
+        SoundFX.Play();
+    }
+
+    /// <summary>
+    /// Stop a SoundFX clip
+    /// </summary>
+    public void Stop()
+    {
+        SoundFX.Stop();
+    }
+
+    /// <summary>
+    /// Change a SoundFX clip
+    /// </summary>
+    /// <param name="clip">Clip.</param>
+    public void ChangeClip(AudioClip clip)
+    {
+        SoundFX.clip = clip;
+    }
+
+    /// <summary>
+    /// Sets the music volume.
+    /// </summary>
+    /// <param name="volume">Volume.</param>
+    public void SetMusicVolume(float volume)
+    {
+        MusicEnvironment.volume = volume;
+    }
+
+    /// <summary>
+    /// Sets the sound FX volume.
+    /// </summary>
+    /// <param name="volume">Volume.</param>
+    public void SetSoundFXVolume(float volume)
+    {
+        SoundFX.volume = volume;
+    }
+}
--- Assets/Script/GUI/Options.cs
+++ Assets/Script/GUI/Options.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Options : StateComponentBase<Menu>
@@ -8,7 +9,22 @@ public class Options : StateComponentBase<Menu>
     private RectTransform CastPanel;
 
     [SerializeField]
+    private Slider MusicSlide;
+
+    [SerializeField]
+    private Slider SoundFXSlide;
+
+    [SerializeField]
     private float SlideSpeed;
+
+    private void Start()
+    {
+        MusicSlide.value = AudioManager.Instance.MusicEnvironment.volume;
+        SoundFXSlide.value = AudioManager.Instance.SoundFX.volume;
+
+        MusicSlide.onValueChanged.AddListener(AudioManager.Instance.SetMusicVolume);
+        SoundFXSlide.onValueChanged.AddListener(AudioManager.Instance.SetSoundFXVolume);
+    }
 
     public override void EnterState()
     {
@@ -18,9 +34,11 @@ public class Options : StateComponentBase<Menu>
     public override void ExitState()
     {
         Behaviour.ToggleButtons(true);
+        PlayerPrefs.SetFloat("Music", MusicSlide.value);
+        PlayerPrefs.SetFloat("SoundFX", SoundFXSlide.value);
     }
 
-    private void Update ()
+    private void Update()
     {
         if(IsActive)
             CastPanel.localPosition = Vector3.Lerp(CastPanel.localPosition, new Vector3(0,0,0), SlideSpeed * Time.deltaTime);
